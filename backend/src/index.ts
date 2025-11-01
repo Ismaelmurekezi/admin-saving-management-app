0d5497d74e6ba1337b076d1b3392a8872854ad96
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2025-11-01 10:21:04 +0200

    chore(config): add user limit setting and provide .env.example file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,9 @@ import { connectDatabase } from "./config/database.js";
 import { specs, swaggerUi } from "./config/swagger.js";
 import adminRoutes from "./routes/adminRoute.js";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+
 
 
 dotenv.config();
@@ -11,12 +14,26 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+//security middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message:
+    "Too many requests from this IP, please try again after a 15 minutes",
+});
+
+app.use(limiter);
+
+
 app.get("/", (request, response) => {
   response.status(200).send("Welcome to the Useve admin  Backend Application!");
 });
 
-app.use(cors());
-app.use(express.json());
 
 //swagger documentation
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
